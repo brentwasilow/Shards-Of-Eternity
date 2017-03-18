4e2bdbffd54a73556cb94433d24a1d46a357b624
Brent Wasilow <brentwasilow2@gmail.com>
2017-03-17 17:59:31 -0700

    Added display class: encapsulates JFrame and Canvas components of our game.

--- src/main/java/com/btwasilow/shardsofeternity/Main.java
+++ src/main/java/com/btwasilow/shardsofeternity/Main.java
@@ -1,8 +1,10 @@
 package com.btwasilow.shardsofeternity;
 
+import com.btwasilow.shardsofeternity.display.Display;
+
 public class Main {
-
+	
 	public static void main(String[] args) {
+		Display display = new Display("Shards of Eternity", 800, ((9*700/16) + 100));
 	}
-
 }
