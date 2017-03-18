4abc13a1d3a8c20f4b158a1e9ea341f645a4a8bd
Brent Wasilow <brentwasilow2@gmail.com>
2017-03-17 19:01:31 -0700

    Added Game class: encapsulates game setup logic such as setting up the display, starting the threads, and calculating the game loop.

--- src/main/java/com/btwasilow/shardsofeternity/Main.java
+++ src/main/java/com/btwasilow/shardsofeternity/Main.java
@@ -1,10 +1,9 @@
 package com.btwasilow.shardsofeternity;
 
-import com.btwasilow.shardsofeternity.display.Display;
-
 public class Main {
 	
 	public static void main(String[] args) {
-		Display display = new Display("Shards of Eternity", 800, ((9*700/16) + 100));
+		Game game = new Game("Shards of Eternity", 800, ((9*700/16) + 100));
+		game.start();
 	}
 }
